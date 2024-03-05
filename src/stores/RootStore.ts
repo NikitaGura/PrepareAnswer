adf7d414779514757df5a70085dcc514b96ff23a
NikitaGura <gura.nikita2017@gmail.com>
2024-03-05 20:25:40 +0100

    remove ModalPopupStore

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,9 +1,7 @@
 import CreateQuestionsStore from './create-question-store';
 import QuestionsStore from './list-questions-store';
-import ModalPopupStore from './modal-popup-store';
 
 class RootStore {
-  modalPopup = new ModalPopupStore();
   questions = new QuestionsStore();
   createQuestions = new CreateQuestionsStore();
 }
